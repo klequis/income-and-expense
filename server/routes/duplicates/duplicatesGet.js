22de11ca28975457900e1bc59f251dee4c0acef1
Carl Becker <klequis@gmail.com>
2020-04-22 08:42:37 -0700

    update error message

--- server/routes/duplicates/duplicatesGet.js
+++ server/routes/duplicates/duplicatesGet.js
@@ -1,14 +1,16 @@
 import wrap from 'routes/wrap'
 import { find } from 'db/dbFunctions'
 import { DATA_COLLECTION_NAME } from 'db/constants'
-import { redf } from 'logger'
+
+// eslint-disable-next-line
+import { redf, yellow } from 'logger'
 
 const duplicatesGet = wrap(async (req, res) => {
   try {
     const data = await find(DATA_COLLECTION_NAME, { duplicate: true })
     res.send(data)
   } catch (e) {
-    redf('views/allDataByDescription', e.message)
+    redf('duplicates/duplicatesGet', e.message)
     console.log(e)
   }
 })
