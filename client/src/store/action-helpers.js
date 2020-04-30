99f5b31b23469d3ed718fa9d8c3ebb2be09f5c81
Carl Becker <klequis@gmail.com>
2020-04-30 12:52:47 -0700

    make use of requestPending inc/dec

--- client/src/store/action-helpers.js
+++ client/src/store/action-helpers.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line
 import {
+  requestPendingCountIncrementAction,
+  requestPendingCountDecrementAction,
   requestPendingAction,
   requestSuccessAction,
-  requestFailedAction
+  requestFailedAction,
 } from './requests/actions'
-// import { useFinanceContext } from 'financeContext'
+import * as R from 'ramda'
 
 // eslint-disable-next-line
 import { pink, red } from 'logger'
@@ -12,6 +14,13 @@ import { pink, red } from 'logger'
 export const logError = (err, key) => {
   red(`actions.logError(key:${key})`, err)
 }
+
+const getKeyFromFunction = (fnStr) => {
+  const l = fnStr.split(/\s+/)[6]
+  const m = R.match(/\[(.*?)\]/, l)
+  return m[1]
+}
+
 
 export const createRequestThunk = ({
   request,
@@ -28,25 +37,27 @@ export const createRequestThunk = ({
   return (...args) => async (dispatch) => {
     // pink('args', args)
     const requestKey = typeof key === 'function' ? key(...args) : key
-    // pink('requestKey', requestKey)
-    start.map(async (actionCreator) => {
-      await dispatch(actionCreator())
-    })
+    // I never use start
+    // start.map(async (actionCreator) => {
+    //   await dispatch(actionCreator())
+    // })
     await dispatch(requestPendingAction(requestKey))
+    await dispatch(requestPendingCountIncrementAction(requestKey))
 
     try {
+      pink('requesting', requestKey)
       const data = await request(...args)
+      pink('returned', requestKey)
       await dispatch(requestSuccessAction(requestKey))
       success.map(async (actionCreator) => {
-        // pink('success.actionCreator', actionCreator)
-        dispatch(requestSuccessAction(requestKey))
-        // pink('success: data', data)
+        pink('dispatching', getKeyFromFunction(actionCreator.toString()))
         await dispatch(actionCreator(data))
+        pink('done', getKeyFromFunction(actionCreator.toString()))
+        dispatch(requestPendingCountDecrementAction(requestKey))
       })
     } catch (e) {
       await dispatch(requestFailedAction(e, requestKey))
       return failure.map(async (actionCreator) => {
-        // pink('failure.actionCreator', actionCreator)
         red('action.helpers.createRequestThunk Error', e.message)
         console.log(e)
         dispatch(requestFailedAction(e, requestKey))
@@ -55,63 +66,3 @@ export const createRequestThunk = ({
     }
   }
 }
-
-// Promise
-// export const createRequestThunk = ({
-//   request,
-//   key,
-//   start = [],
-//   success = [],
-//   failure = []
-// }) => {
-//   return (...args) => dispatch => {
-//     const requestKey = typeof key === 'function' ? key(...args) : key
-//     start.forEach(actionCreator => dispatch(actionCreator()))
-//     dispatch(requestPending(requestKey))
-
-//     return request(...args)
-//       .then(data => {
-//         success.forEach(actionCreator => dispatch(actionCreator(data)))
-//         dispatch(requestSuccess(requestKey))
-//       })
-//       .catch(reason => {
-//         failure.forEach(actionCreator => {
-//           pink('failure.actionCreator', actionCreator)
-//           dispatch(actionCreator(reason))
-//         })
-//         dispatch(requestFailed(reason, requestKey))
-//       })
-//   }
-// }
-
-// ORIG
-// export const createRequestThunk = ({
-//   request,
-//   key,
-//   start = [],
-//   success = [],
-//   failure = []
-// }) => {
-//   return (...args) => async dispatch => {
-//     const requestKey = typeof key === 'function' ? key(...args) : key
-//     start.map(async actionCreator => {
-//       await dispatch(actionCreator())
-//     })
-//     await dispatch(requestPending(requestKey))
-//     try {
-//       const data = await request(...args)
-//       await dispatch(requestSuccess(requestKey))
-//       success.map(async actionCreator => {
-//         await dispatch(actionCreator(data))
-//         dispatch(requestSuccess(requestKey))
-//       })
-//     } catch (e) {
-
-//       await dispatch(requestFailed(e, requestKey))
-//       failure.map(async actionCreator => {
-//         await dispatch(actionCreator(e))
-//         dispatch(requestFailed(e, requestKey))
-//       })
-//     }
-//   }
-// }
