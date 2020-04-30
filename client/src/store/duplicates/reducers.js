d191d404ebe3af1bb5ba0ac125358e60a18c3848
Carl Becker <klequis@gmail.com>
2020-04-30 12:53:38 -0700

    add logging

--- client/src/store/duplicates/reducers.js
+++ client/src/store/duplicates/reducers.js
@@ -3,9 +3,12 @@ import {
   // DUPLICATES_UPDATE_KEY,
 } from './constants'
 
+import { blue } from 'logger'
+
 export const duplicatesReducer = (state= [], { type, payload }) => {
   switch (type) {
     case DUPLICATES_READ_KEY:
+      blue('duplicates.length', payload.length)
       return payload
     default:
       return state
