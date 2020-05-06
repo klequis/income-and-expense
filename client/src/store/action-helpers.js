813565cac6fe2c329792cb99ebb432e1f4adc54c
Carl Becker <klequis@gmail.com>
2020-05-06 12:54:17 -0700

    with commented out IF statements

--- client/src/store/action-helpers.js
+++ client/src/store/action-helpers.js
@@ -22,58 +22,94 @@ const getKeyFromFunction = (fnStr) => {
   return m[1]
 }
 
+const getActionKey = action => R.type(action) === 'Object'
+  ? action.key
+  : action.request.key
+    
+
+const getActionFn = action => R.type(action) === 'Object'
+  ? action.fn
+  : action.request.fn
 
 export const createRequestThunk = ({
   request,
   success = [],
   failure = []
 }) => {
-  return (...args) => async (dispatch) => {
-    console.group('createRequestThunk')
-    pink('request.key', request.key)
-    pink('typeof request.fn', request.fn)
-    pink('success', success)
-    pink('failure', failure)
-    
+  console.group('request types')
+  pink('REQUEST', request)
+  pink('typeof REQUEST', R.type(request))
+  pink('SUCCESS', success)
+  console.groupEnd()
+  const returnFn = (...args) => async (dispatch) => {
+    // console.group('createRequestThunk')
+    // pink('request.key', request.key)
+    // pink('typeof request.fn', request.fn)
+    // pink('success', success)
+    // pink('failure', failure)
 
-    // if (!R.has('key')(request)) {
-    //   throw new Error(`request missing key`)
-    // }
+    const _dispatch = action => {
 
-    // add request and success keys here OR
-    // add all keys here and put a finally?
+    }
+
     dispatch(actionsPendingAdd.fn(request.key))
     success.forEach(s => {
-      pink('success key', s.key)
-      dispatch(actionsPendingAdd.fn(s.key))
+      // if (R.type(s) === 'Object') {
+      //   dispatch(actionsPendingAdd.fn(s.key))
+      // } else {
+      //   dispatch(actionsPendingAdd.fn(s.request.key))
+      // }
+      dispatch(actionsPendingAdd.fn(getActionKey(s)))
     })
     failure.forEach(f => {
-      pink('failure key', f.key)
-      dispatch(actionsPendingAdd.fn(f.key))
+      // if (R.type(f) === 'Object') {
+      //   dispatch(actionsPendingAdd.fn(f.key))
+      // } else {
+      //   dispatch(actionsPendingAdd.fn(f.request.key))
+      // }
+      dispatch(actionsPendingAdd.fn(getActionKey(f)))
     })
-    console.groupEnd()
+    // console.groupEnd()
     try {
       dispatch(requestPendingAction.fn(request.key))
       // dispatch(actionsPendingAdd.fn(request.key))
       const data = await request.fn(...args)
       dispatch(requestSuccessAction.fn(request.key))
-      success.map(async (actionCreator) => {
-        dispatch(actionCreator.fn(data))
+      success.map(async (s) => {
+        // if (R.type(actionCreator) === 'Object') {
+        //   dispatch(actionCreator.fn(data))
+        // } else {
+        //   dispatch(actionCreator.request.fn(data))
+        // }
+        dispatch(getActionFn(s)(data))
+        // red('typeof actionCreator', R.type(actionCreator))
+        
         // dispatch(actionsPendingRemove.fn(request.key))
       })
     } catch (e) {
+      
       dispatch(requestFailedAction.fn(e, request.key))
-      // return failure.map(async (actionCreator) => {
-      //   red('action.helpers.createRequestThunk Error', e.message)
-      //   console.log(e)
-      //   dispatch(requestFailedAction.fn(e, request.key))
-      //   await dispatch(actionCreator(e))
-      // })
+      red('action.helpers.createRequestThunk Error', e.message)
+      console.log(e)
+      return failure.map(async (f) => {
+        // if (R.type(actionCreator === 'Object')) {
+        //   dispatch(actionCreator.fn(e, actionCreator.key))
+        // } else {
+        //   dispatch(actionCreator.request.fn(e, actionCreator.request.key))
+
+        //   // don't know what this last line was for before 
+        //   // most recent revision. 
+        //   // await dispatch(actionCreator(e))
+        // }
+        dispatch(getActionFn(f)(e, getActionKey(f)))
+      })
     } finally {
       red('FINALLY')
       dispatch(actionsPendingRemove.fn(request.key))
-      success.forEach(s => dispatch(actionsPendingRemove.fn(s.key)))
-      failure.forEach(f => dispatch(actionsPendingRemove.fn(f.key)))
+      success.forEach(s => dispatch(actionsPendingRemove.fn(getActionKey(s))))
+      failure.forEach(f => dispatch(actionsPendingRemove.fn(getActionKey(f))))
     }
   }
+  // pink('returnFn', returnFn)
+  return returnFn
 }
