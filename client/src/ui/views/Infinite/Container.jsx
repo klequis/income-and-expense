fac8e10fa56fc51c86b44ef6a5fe65bd749de1d6
klequis <klequis@gmail.com>
2020-07-22 07:53:00 -0700

    debugging -wip

--- client/src/ui/views/Infinite/Container.jsx
+++ client/src/ui/views/Infinite/Container.jsx
@@ -33,23 +33,38 @@ const Container = () => {
     _setLoading(true)
     ;(async () => {
       await refreshRulesAndView(views.allDataByDescription)
-      _setLoading(false)
     })()
+    _setLoading(false)
     
     // eslint-disable-next-line
   }, [])
 
   const _viewData = useSelector(state => state.viewData)
+
+  green('BEFORE: _viewData.length', _viewData.length)
+  // Works for initial load
   if (_viewData.length === 0) {
-    return <h1>Loading</h1>
+    green('LOADING 1b')
+    return <h1>Loading 1</h1>
   }
-  green('_viewData.length', _viewData.length)
-  if (_loading && (
-      actionIsPending(actionKeys.viewReadKey) || actionIsPending(actionKeys.rulesReadKey))
-    ) {
-    green('LOADING')
-    return <h1>Loading</h1>
-  }
+  green('AFTER: _viewData.length', _viewData.length)
+
+
+  /*
+      NEXT: compare numbers of renders with and without below code where a render would be reaching the `green('container)` line above `return`.
+
+      DONE: there is no difference :(
+  */
+
+
+  // works for reload BUT seems not needed?
+  
+  // if (_loading && (
+  //     actionIsPending(actionKeys.viewReadKey) || actionIsPending(actionKeys.rulesReadKey))
+  //   ) {
+  //   green('LOADING 2d')
+  //   return <h1>Loading</h1>
+  // }
 
 
   green('container')
